03ea5899ad658559c4735589ac54b070b1c87e26
Devashree Shet Shirodkar <87293422+DevashriShirodkar@users.noreply.github.com>
2024-01-23 06:22:11 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,10 @@
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true / false
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+console.log(typeof undefined) //undefined
+console.log(typeof null) //object
